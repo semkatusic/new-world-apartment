bdc7ece533e3519705f495c6969c3d6c5495abac
semkatusic <semkatusic1@gmail.com>
2026-04-28 02:22:14 +0200

    Create calendar.ics.js

---
+++ api/calendar.ics.js
@@ -0,0 +1,35 @@
+module.exports = async (req, res) => {
+  try {
+    const response = await fetch(process.env.GOOGLE_SCRIPT_URL);
+    const data = await response.json();
+
+    const events = data
+      .filter(b => b.status === "confirmed")
+      .map(b => {
+        const start = b.arrival.replaceAll("-", "");
+        const end = b.departure.replaceAll("-", "");
+
+        return `
+BEGIN:VEVENT
+UID:${b.booking_id}@newworldapartment
+DTSTAMP:${new Date().toISOString().replace(/[-:]/g, "").split(".")[0]}Z
+DTSTART;VALUE=DATE:${start}
+DTEND;VALUE=DATE:${end}
+SUMMARY:Blocked - New World Apartment
+END:VEVENT`;
+      })
+      .join("");
+
+    const ics = `BEGIN:VCALENDAR
+VERSION:2.0
+PRODID:-//New World Apartment//Booking Calendar//EN
+CALSCALE:GREGORIAN
+${events}
+END:VCALENDAR`;
+
+    res.setHeader("Content-Type", "text/calendar");
+    res.status(200).send(ics);
+  } catch (error) {
+    res.status(500).send("Calendar error");
+  }
+};
